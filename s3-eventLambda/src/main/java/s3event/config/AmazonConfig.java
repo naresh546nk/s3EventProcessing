29c0b18b7444b8cee833d2cf0912f730bce835aa
naresh546nk <91820009+naresh546nk@users.noreply.github.com>
2022-08-15 08:25:42 +0530

    added sucurity

--- s3-eventLambda/src/main/java/s3event/config/AmazonConfig.java
+++ s3-eventLambda/src/main/java/s3event/config/AmazonConfig.java
@@ -1,7 +1,4 @@
 package s3event.config;
-
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -11,12 +8,10 @@ import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 
 public class AmazonConfig {
-    private static  String accessKey="AKIA3TAHJ5R4PO2S3Y4L";
-    private static  String secretKey="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";
     public static  AmazonS3 getAmazonS3Client(){
       return  AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.AP_SOUTH_1)
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,secretKey)))
+                //.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,secretKey)))
                 .build();
 
     }
@@ -25,7 +20,7 @@ public class AmazonConfig {
         return  AmazonDynamoDBClientBuilder
                 .standard()
                 .withRegion(Regions.AP_SOUTH_1)
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,secretKey)))
+                //.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,secretKey)))
                 .build();
 
     }
@@ -34,7 +29,7 @@ public class AmazonConfig {
         return AmazonSQSClientBuilder
                 .standard()
                 .withRegion(Regions.AP_SOUTH_1)
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,secretKey)))
+               // .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,secretKey)))
                 .build();
     }
 }
